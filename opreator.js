d0c98c6ea1c9b8ca8461c3460ed6077227d04cba
neophyte_0903 <gagandeep.21gcebaids001@galgotiacollege.edu>
2022-12-10 20:01:54 +0530

    new program added

---
+++ opreator.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let num=7;
+let num2="7";
+
+console.log(num!==num2);
+// greater >
+//lesser >
+// equality == (doesnt check for the type of the object )
+// equality === (data type is also checked in this form)
