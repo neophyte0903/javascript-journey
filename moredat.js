a6c642f5c664c06d7fda92017e79726c1244c5ff
neophyte_0903 <gagandeep.21gcebaids001@galgotiacollege.edu>
2022-12-10 19:17:58 +0530

    new files added

--- moredat.js
+++ moredat.js
@@ -1,6 +1,12 @@
 //undefined
-//null
+let firstName;
 
+console.log(typeof firstName)
+//null
+firstName=null;
+console.log(typeof firstName) //here is a bug it cant be changed for conviniece
 
 
 //BigInt
+firstName=BigInt(1222222222222222444444444445555);
+console.log(typeof firstName,firstName)
